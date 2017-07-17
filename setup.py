0c50a8f3fbf11348867ac0e0bcd4c9de807a5bf2
Xiaoyong Pan <xypan172436@gmail.com>
2017-07-17 23:12:03 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+config = {
+    'description': 'iDeepE: inferring protein-RNA binding sites and motifs using local and global CNNs',
+    'download_url': 'https://github.com/xypan1232/iDeepE',
+    'version': '0.1.0',
+    'packages': ['iDeepE'],
+    'setup_requires': [],
+    'install_requires': ['numpy>=1.9', 'scikit-learn', 'PyTorch==0.1.11'],
+    'scripts': [],
+    'name': 'iDeepE'
+}
+
+if __name__== '__main__':
+    setup(**config)
