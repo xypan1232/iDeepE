ba255215bf65301c47591f922b4711c201eebf73
Xiaoyong Pan <Xiaoyong Pan>
2017-07-27 14:22:35 +0200

    update

--- ideepe.py
+++ ideepe.py
@@ -581,9 +581,9 @@ class _DenseLayer(nn.Sequential):
         self.add_module('relu.1', nn.ReLU(inplace=True)),
         self.add_module('conv.1', nn.Conv2d(num_input_features, bn_size *
             growth_rate, kernel_size=1, stride=1, bias=False)),
-            self.add_module('norm.2', nn.BatchNorm2d(bn_size * growth_rate)),
-            self.add_module('relu.2', nn.ReLU(inplace=True)),
-            self.add_module('conv.2', nn.Conv2d(bn_size * growth_rate, growth_rate,
+        self.add_module('norm.2', nn.BatchNorm2d(bn_size * growth_rate)),
+        self.add_module('relu.2', nn.ReLU(inplace=True)),
+        self.add_module('conv.2', nn.Conv2d(bn_size * growth_rate, growth_rate,
             kernel_size=(1, 3), stride=1, padding=(0, 1), bias=False)),
         self.drop_rate = drop_rate
 
